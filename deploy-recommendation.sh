748d125fcd3956d15a1fad430c47fc70920e998f
Kamesh Sampath <kamesh.sampath@hotmail.com>
2019-03-16 22:21:33 +0530

    (fix): fix script flow
    - updated docs
    - fixed script flow

--- deploy-recommendation.sh
+++ deploy-recommendation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 # TODO revisit
 
-set -eu
+set -e
 
 TUTORIAL_HOME='/Users/kameshs/git/redhat-developer-demos/istio-tutorial'
 
@@ -25,15 +25,18 @@ if [ "$_clean" = "yes" ];
 then 
  ! kubectl delete  -n tutorial -f  "recommendation-build.yaml"
  ! kubectl delete  -n tutorial -f "$TUTORIAL_HOME/recommendation/kubernetes/Service.yml"
- kubectl create -n tutorial  -f "$TUTORIAL_HOME/recommendation/kubernetes/Service.yml"
- # Set service to be node port
- kubectl -n tutorial get svc recommendation -o yaml | yq w - spec.type NodePort | kubectl apply -f -
+ ! kubectl delete -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment.yml" -n tutorial
+ ! kubectl delete -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment-v2.yml" -n tutorial
+ exit 0
 fi
+
+! kubectl create -n tutorial  -f "$TUTORIAL_HOME/recommendation/kubernetes/Service.yml"
+# Set service to be node port
+! kubectl -n tutorial get svc recommendation -o yaml | yq w - spec.type NodePort | kubectl apply -f -
 
 if [ "$_version" = 'v1' ]; 
 then 
- ! kubectl delete  -n tutorial -f  "recommendation-build.yaml"
- [ "$_clean" = "yes" ] && ! kubectl delete -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment.yml" -n tutorial
+! kubectl delete  -n tutorial -f  "recommendation-build.yaml"
  sed 's|--imgTag--|v1|; s|--gitRev--|master|' recommendation-build.yaml |  kubectl apply -n tutorial -f - 
  kubectl apply -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment.yml" -n tutorial
 fi
@@ -42,7 +45,6 @@ if [ "$_version" = 'v2' ];
 then 
  ! kubectl delete  -n tutorial -f  "recommendation-build.yaml"
  sed 's|--imgTag--|v2|; s|--gitRev--|recommendation-v2|' recommendation-build.yaml |  kubectl apply  -n tutorial -f -
- [ "$_clean" = "yes" ] && ! kubectl delete -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment-v2.yml" -n tutorial
  kubectl apply -f "$TUTORIAL_HOME/recommendation/kubernetes/Deployment-v2.yml" -n tutorial
 fi
 
